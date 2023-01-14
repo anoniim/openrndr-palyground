4e786f1e305357bbb8cb0be7a9a5ff67405f508d
Marcel Bro <marcel.broz@gmail.com>
2023-01-14 22:12:47 +0000

    Sketch size + position for LG Ultrawide monitor

--- src/main/kotlin/Customizations.kt
+++ src/main/kotlin/Customizations.kt
@@ -1,13 +1,13 @@
 import org.openrndr.Configuration
 import org.openrndr.math.IntVector2
 
-fun Configuration.display(display: MyDisplay) {
+fun Configuration.sketchSize(display: Displays) {
     width = display.width
     height = display.height
     position = IntVector2(display.xPosition, 0)
 
 }
 
-enum class MyDisplay(val width: Int, val height: Int, val xPosition: Int) {
+enum class Displays(val width: Int, val height: Int, val xPosition: Int) {
     LG_ULTRAWIDE(2560, 1550, 1280)
 }
--- src/main/kotlin/experiments/ScreenPosition.kt
+++ src/main/kotlin/experiments/ScreenPosition.kt
@@ -1,18 +1,15 @@
 package experiments
 
-import display
-import org.openrndr.ApplicationBuilder
-import org.openrndr.Configuration
+import sketchSize
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
-import org.openrndr.math.IntVector2
 
 fun main() = application {
     configure {
 //        width = 2560 // 1/3 of 3440 = 1146
 //        height = 1550
 //        position = IntVector2(1280, 0)
-        display(MyDisplay.LG_ULTRAWIDE)
+        sketchSize(Displays.LG_ULTRAWIDE)
 
 //        windowResizable = true
         title = ""
