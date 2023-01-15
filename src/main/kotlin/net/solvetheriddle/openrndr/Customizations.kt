7e9f5a0804a4eab0b99c7807115ab357b5161d77
Marcel Bro <marcel.broz@gmail.com>
2023-01-15 01:16:32 +0000

    Fullscreen syntax improved

--- src/main/kotlin/net/solvetheriddle/openrndr/Customizations.kt
+++ src/main/kotlin/net/solvetheriddle/openrndr/Customizations.kt
@@ -12,16 +12,16 @@ fun Configuration.sketchSize(config: SketchSizeConfig) {
         height = config.height
         position = IntVector2(config.xPosition, 0)
 
-    } else if (config == SketchSizeConfig.FULLSCREEN) {
+    } else if (config == Display.FULLSCREEN) {
         fullscreen = Fullscreen.CURRENT_DISPLAY_MODE
     }
 }
 
-sealed interface SketchSizeConfig {
-    object FULLSCREEN: SketchSizeConfig
-}
+interface SketchSizeConfig
 
 enum class Display(val width: Int, val height: Int, val xPosition: Int): SketchSizeConfig {
     LG_ULTRAWIDE(2560, 1550, 1280),
     MACBOOK_AIR(1500, 997, 500),
+    ;
+    object FULLSCREEN: SketchSizeConfig
 }
