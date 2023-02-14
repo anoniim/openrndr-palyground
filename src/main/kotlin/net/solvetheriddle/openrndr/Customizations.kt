9ca3e2bb29e6024ff4f724befb40f520f6ed28c3
Marcel Bro <marcel.broz@gmail.com>
2023-02-14 21:03:03 +0000

    Square sketch position for LG monitor

--- src/main/kotlin/net/solvetheriddle/openrndr/Customizations.kt
+++ src/main/kotlin/net/solvetheriddle/openrndr/Customizations.kt
@@ -10,7 +10,7 @@ fun Configuration.sketchSize(config: SketchSizeConfig) {
     if (config is Display) {
         width = config.width
         height = config.height
-        position = IntVector2(config.xPosition, 0)
+        position = IntVector2(config.xPosition, config.yPosition)
 
     } else if (config == Display.FULLSCREEN) {
         fullscreen = Fullscreen.CURRENT_DISPLAY_MODE
@@ -19,10 +19,11 @@ fun Configuration.sketchSize(config: SketchSizeConfig) {
 
 interface SketchSizeConfig
 
-enum class Display(val width: Int, val height: Int, val xPosition: Int): SketchSizeConfig {
+enum class Display(val width: Int, val height: Int, val xPosition: Int, val yPosition: Int = 0): SketchSizeConfig {
     LG_ULTRAWIDE(2560, 1550, 1280),
     LG_ULTRAWIDE_LEFT(2560, 1550, 0),
     MACBOOK_AIR(1500, 997, 500),
+    LG_SQUARE_LEFT(1080, 1080, (Display.LG_ULTRAWIDE.height - 1080) / 2, 100),
     ;
     object FULLSCREEN: SketchSizeConfig
 }
