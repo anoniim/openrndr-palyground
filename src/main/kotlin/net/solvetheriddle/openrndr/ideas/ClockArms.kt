24e3bf65559bb00855e061055f121ae2906b7ee4
Marcel Bro <marcel.broz@gmail.com>
2023-02-19 11:28:24 +0000

    Clock Arms reverted to keep the code that actually looks somewhat like a clock

--- src/main/kotlin/net/solvetheriddle/openrndr/ideas/ClockArms.kt
+++ src/main/kotlin/net/solvetheriddle/openrndr/ideas/ClockArms.kt
@@ -8,11 +8,9 @@ import org.openrndr.Program
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.extra.color.presets.DARK_GOLDEN_ROD
+import org.openrndr.math.Polar
 import org.openrndr.math.Vector2
 import org.openrndr.shape.LineSegment
-import kotlin.math.abs
-import kotlin.math.floor
-import kotlin.math.round
 
 fun main() = application {
     configure {
@@ -20,30 +18,21 @@ fun main() = application {
     }
     program {
         val armLength = 400.0
-        val numOfArms = 15
-        val framesPerTick = 10
-        var tickPointer = 0
+        val numOfArms = 50
+        val revolutionLength = 60 * 4
+        var tickPointer = 3 * numOfArms / 4
         val tickAlpha = 2.0
-        val attack = 0.5
-        val decay = 0.020
 
         val arms = List(numOfArms) {
-            val lineStart = Vector2(width/(numOfArms + 2.0) * (it + 2), height/4.0)
-            val lineEnd = Vector2(width/(numOfArms + 2.0) * (it + 2), 3 * height/4.0)
-            Arm(lineStart, lineEnd, tickAlpha, attack, decay)
+            val lineEnd = Polar(360.0 / numOfArms * it, armLength).cartesian
+            Arm(Vector2.ZERO, lineEnd, tickAlpha, 0.01)
         }
 
         val movie = Movie().apply {
-            append(Move(framesPerTick) { frameCount ->
-
-                // FIXME attempt for triangle wave function
-                val tickPointer1 = round(10 * abs(frameCount/(framesPerTick * 2.0) - (floor(frameCount/(framesPerTick * 2.0) + 1/2.0))))
-                println(tickPointer1)
-
-                if (frameCount % framesPerTick == 0) {
-                    tickPointer = (tickPointer + 1) % (numOfArms - 1)
-                    arms[tickPointer].show()
-                }
+            append(Move(revolutionLength) { frameCount ->
+                println(frameCount)
+                val ticksPerFrame = if (numOfArms < revolutionLength) revolutionLength / numOfArms else 1
+                if (frameCount % ticksPerFrame == 0) arms[tickPointer++ % numOfArms].show()
                 arms.forEach {
                     it.update(frameCount)
                     it.draw()
@@ -52,7 +41,7 @@ fun main() = application {
         }
 
         extend {
-//            drawer.translate(drawer.bounds.center)
+            drawer.translate(drawer.bounds.center)
             movie.play()
         }
     }
@@ -62,41 +51,25 @@ fun main() = application {
 private class Arm(
     start: Vector2,
     end: Vector2,
-    private val tickAlpha: Double = 1.0,
-    private val attack: Double = 0.5,
+    private val tickAlpha: Double = 2.0,
     private val decay: Double = 0.01,
-    val strokeWeight: Double = 40.0,
 ) {
 
     private val lineSegment = LineSegment(start, end)
     private var alpha = 0.0
-    private var state = State.DEFAULT
 
     fun show() {
-        state = State.ATTACK
+        alpha = tickAlpha
     }
 
     fun update(frameCount: Int) {
-        if (state == State.ATTACK) {
-            alpha += attack
-            if (alpha >= tickAlpha) {
-                alpha = tickAlpha
-                state = State.DECAY
-            }
-        }
-        if (state == State.DECAY && alpha > 0) alpha -= decay
+        if (alpha > 0) alpha -= decay
     }
 
     context(Program)
     fun draw() {
-        drawer.strokeWeight = strokeWeight
-        drawer.stroke = ColorRGBa.DARK_GOLDEN_ROD.copy(alpha = alpha)
+        drawer.strokeWeight = 3.0
+        drawer.stroke = ColorRGBa.DARK_GOLDEN_ROD.opacify(alpha)
         drawer.lineSegment(lineSegment)
-    }
-
-    enum class State {
-        DEFAULT,
-        ATTACK,
-        DECAY,
     }
 }
