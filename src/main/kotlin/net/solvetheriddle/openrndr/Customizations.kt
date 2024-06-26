d79271d50352f4971a081a52b3ad793980b090a4
Marcel Bro <marcel.broz@gmail.com>
2024-06-27 00:35:50 +0100

    Display options updated * A6 print format added
    Signed-off-by: Marcel Bro <marcel.broz@gmail.com>

--- src/main/kotlin/net/solvetheriddle/openrndr/Customizations.kt
+++ src/main/kotlin/net/solvetheriddle/openrndr/Customizations.kt
@@ -22,9 +22,10 @@ interface SketchSizeConfig
 enum class Display(val width: Int, val height: Int, val xPosition: Int, val yPosition: Int = 0): SketchSizeConfig {
     LG_ULTRAWIDE(2560, 1550, 1280),
     LG_ULTRAWIDE_LEFT(2560, 1550, 0),
-    MACBOOK_AIR(1500, 997, 500),
     LG_SQUARE_LEFT(1080, 1080, 100, (LG_ULTRAWIDE.height - 1080) / 2),
     LG_PORTRAIT_LEFT(630, 1305, 325, (LG_ULTRAWIDE.height - 1305) / 2),
+    MACBOOK_AIR(1500, 997, 500),
+    PRINT_A6(1485,1050,0),
     ;
     object FULLSCREEN: SketchSizeConfig
 }
